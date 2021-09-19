e4d0cab217a581198c2cc9ab4d65c57e78218035
Carlos3108 <c.silva1609@gmail.com>
2021-09-19 03:56:07 -0300

    Criacao de Tela de Login Finalizada

---
+++ app/src/main/java/com/example/testecomanda/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.testecomanda;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TextView cpf = findViewById(R.id.textCpfLogin);
+        TextView password = findViewById(R.id.textPassword);
+        Button login = findViewById(R.id.buttonLogin);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                validate(cpf.getText().toString(), password.getText().toString());
+            }
+        });
+    }
+    public void validate(String cpf, String password) {
+        TextView texto = findViewById(R.id.textView);
+        if (cpf.equals("admin") && password.equals("admin")){
+            Intent intent = new Intent(MainActivity.this, Mesa.class);
+            startActivity(intent);
+        } else {
+            texto.setText("Erro");
+        }
+    }
+}
---
+++ app/src/main/java/com/example/testecomanda/Mesa.java
@@ -0,0 +1,14 @@
+package com.example.testecomanda;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Mesa extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mesa);
+    }
+}
