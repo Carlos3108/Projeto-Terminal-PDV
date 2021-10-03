9831e674c2be6f46be73256f903314cf2e31a7d3
Carlos3108 <c.silva1609@gmail.com>
2021-10-03 17:54:57 -0300

    Requisicoes finalizadas

--- app/src/main/java/com/example/testecomanda/Comanda.java
+++ app/src/main/java/com/example/testecomanda/Comanda.java
@@ -1,56 +1,69 @@
 package com.example.testecomanda;
 
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import com.android.volley.Request;
+import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
 import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 
 import org.json.JSONObject;
 
-import java.util.Calendar;
-import java.util.Currency;
-import java.util.Objects;
+import java.util.HashMap;
 
 public class Comanda extends AppCompatActivity {
     private Button buttonAdd;
     private TextView itemComanda;
-    private TextView textView;
-    String a = "";
+    private EditText codigoComanda;
+    private EditText codigoProduto;
+    private EditText quantidadeProdutos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_comanda);
         buttonAdd = (Button) findViewById(R.id.buttonAdd);
-        itemComanda = (TextView) findViewById(R.id.itemComanda);
-        //textView = (TextView) findViewById(R.id.textView);
-        TextView textView = (TextView) findViewById(R.id.textView);
-    }
-        private static Currency MySingleton;
+        TextView itemComanda = findViewById(R.id.itemComanda);
+        TextView codigoComanda = findViewById(R.id.codigoComanda);
+        TextView codigoProduto = findViewById(R.id.codigoProduto);
+        TextView quantidadeProdutos = findViewById(R.id.quantidadeProdutos);
 
-        public void obterUsuario(String nome){
-            String URL = "http://31.220.21.132:5000/usuario/";
-            JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
-                    (Request.Method.GET, URL + nome, null, new Response.Listener<JSONObject>() {
-                        @Override
-                        public void onResponse(JSONObject response) {
-                         itemComanda.setText("Response: " + response.toString());
-                        }
-                    }, new Response.ErrorListener(){
-                        @Override
-                        public void onErrorResponse(VolleyError error) {
 
-                        }
-                    });
-            //MySingleton.getInstance(String.valueOf(this)).addToRequestQueue(jsonObjectRequest);
-        }
+        buttonAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                inputComanda(codigoComanda.getText().toString(), codigoProduto.getText().toString(), quantidadeProdutos.getText().toString());
+            }
+        });
     }
 
+    public void inputComanda(String codComanda, String codProduto, String qtdProd) {
+        RequestQueue queue = Volley.newRequestQueue(this);
+        final String URL = "https://kind-puma-61.loca.lt/comanda/"+codComanda;
+        System.out.println(URL);
+        HashMap<String, String> params = new HashMap<String, String>();
+        params.put("codprod", codProduto);
+        params.put("qtde", qtdProd);
+        JsonObjectRequest request_json = new JsonObjectRequest(URL, new JSONObject(params),
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        System.out.println(response);
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                VolleyLog.e("Error ", error.getMessage());
+            }
+        });
+        queue.add(request_json);
+    }
+}
--- app/src/main/java/com/example/testecomanda/MainActivity.java
+++ app/src/main/java/com/example/testecomanda/MainActivity.java
@@ -3,15 +3,33 @@ package com.example.testecomanda;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,17 +40,43 @@ public class MainActivity extends AppCompatActivity {
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                validate(cpf.getText().toString(), password.getText().toString());
+                obterUsuario(cpf.getText().toString(), password.getText().toString());
+
             }
         });
     }
-    public void validate(String cpf, String password) {
+       public void validate(String login, String password,String senha) {
         TextView texto = findViewById(R.id.textView);
-        if (cpf.toLowerCase(Locale.ROOT).equals("admin") && password.equals("admin")){
+        if(login.equals(login)&&password.equals(senha)){
             Intent intent = new Intent(MainActivity.this, Menu.class);
             startActivity(intent);
-        } else {
-            texto.setText("CPF ou Senha invalidos!");
+           }
+        else {
+            texto.setText("Login ou Senha invalidos!");
         }
+    }
+    public void obterUsuario(String login,String password){
+        TextView texto = findViewById(R.id.textView);
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String URL = "https://kind-puma-61.loca.lt/usuario/"+login;
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
+                (Request.Method.GET, URL, null, new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try{
+                            String senha = response.getString("senha").toString();
+                            System.out.println(senha);
+                            validate(login, password, senha);
+                        }catch(JSONException error){
+                            texto.setText("Login ou Senha invalidos!");
+                        }
+                    }
+                }, new Response.ErrorListener(){
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        texto.setText("Login ou Senha invalidos!");
+                    }
+                });
+        queue.add(jsonObjectRequest);
     }
 }
