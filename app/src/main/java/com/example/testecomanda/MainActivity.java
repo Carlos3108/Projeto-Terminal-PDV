255efb0a2393efee1784ba8222e71bbfbda1f137
Carlos3108 <c.silva1609@gmail.com>
2021-10-03 18:21:03 -0300

    Requisicoes e tela de login finalizada

--- app/src/main/java/com/example/testecomanda/MainActivity.java
+++ app/src/main/java/com/example/testecomanda/MainActivity.java
@@ -3,9 +3,7 @@ package com.example.testecomanda;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -19,15 +17,6 @@ import com.android.volley.toolbox.Volley;
 
 import org.json.JSONException;
 import org.json.JSONObject;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
     @Override
